7cc41fee26d7711762f9eadaa731a21eb21a0f5e
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-09-02 11:58:56 +0300

    modified models.py

--- airport/models.py
+++ airport/models.py
@@ -48,6 +48,10 @@ class Crew(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Flight(models.Model):
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
